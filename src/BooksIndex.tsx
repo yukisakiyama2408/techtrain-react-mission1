b45b86d75d1256ba8a02a14d26b5b45baf77f56d
Sakiyama Yuki <yuki.king.0422@gmail.com>
2022-04-02 22:44:48 +0900

    update
    書籍一覧取得API（認証あり）を追加。まだ未完成

--- src/BooksIndex.tsx
+++ src/BooksIndex.tsx
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+type Book = {
+  Bearer: string;
+};
+
 const BookIndex = () => {
   const urlBooksApi =
-    "https://api-for-missions-and-railways.herokuapp.com/books";
+    "https://api-for-missions-and-railways.herokuapp.com/books?offset=5";
+  const api_token = "react-book-api";
   const [books, setBooks] = useState([]);
   useEffect(() => {
-    axios.get(urlBooksApi).then((res) => {
-      setBooks(res.data);
-    });
+    axios
+      .get(urlBooksApi, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${api_token}`,
+        },
+      })
+      .then((res) => {
+        setBooks(res.data);
+      });
   }, []);
   return (
     <div>
       <h2>本一覧</h2>
+      <div>
+        {books.map((data) => (
+          <div key={data}>aaa</div>
+        ))}
+      </div>
     </div>
   );
 };
