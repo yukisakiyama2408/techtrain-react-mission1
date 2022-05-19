c4bfed511ca5b2aadb0ad59fb5913c70deeb6f88
Sakiyama Yuki <yuki.king.0422@gmail.com>
2022-05-19 18:02:24 +0900

    update
    ホーム画面の修正

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
 const Home = () => {
   return (
-    <>
-      <h1>ホーム</h1>
+    <div>
+      <h1>Book Reveiw</h1>
+      <h2>このアプリについて</h2>
+      <p>
+        このアプリはユーザーの皆さんが読んだ本のレビューを登録したり、他の人のレビューを読んだりできます。是非読んだ本の記録や気になる本を探す際にご利用ください。
+      </p>
       <div>
-        新規登録は<Link to={`/register/`}>こちら</Link>
+        <p>初めての方は</p>
+        <Button variant="contained" component={Link} to="/signup">
+          新規登録
+        </Button>
       </div>
-    </>
+      <div>
+        <p>ユーザーの方はこちら</p>
+        <Button variant="contained" component={Link} to="/login">
+          ログイン
+        </Button>
+      </div>
+    </div>
   );
 };
 
